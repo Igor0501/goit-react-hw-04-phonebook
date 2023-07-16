8bc0e0d54a9f915f38c46c00c7db596388d6a6f7
Igor0501 <s.igor0501@gmail.com>
2023-07-16 13:44:32 +0300

    pull

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,77 @@
-export const App = () => {
+import React, { useEffect, useRef, useState } from 'react';
+
+import { Container, Ul } from './App.styled';
+
+import Title from './Title/Title';
+import Form from './Form/Form';
+import Contact from './Contact/Contact';
+import Filter from './Filter/Filter';
+import { Notify } from 'notiflix';
+
+const App = props => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+  const isFirstRender = useRef(true)
+
+  useEffect(() => {
+    setContacts(JSON.parse(localStorage.getItem('contacts')) || []);
+  }, []);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false
+      return
+    }
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addNewContact = (id, name, number) => {
+    const isContactExist = contacts.some(
+      contact => contact.name.toLocaleLowerCase() === name.toLocaleLowerCase()
+    );
+    if (!isContactExist) {
+      setContacts(prev => [{ id, name, number },  ...prev]);
+    }
+    else {
+      Notify.warning('Sorry, but this NAME has already exist!')
+    }
+  };
+
+  const onFilterChange = e => {
+    const value = e.target.value.toLowerCase();
+    setFilter(value);
+  };
+
+  const onActiveFilter = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter)
+    );
+  };
+
+  const deleteContact = id => {
+    setContacts(prevState => {
+      return prevState.filter(contact => contact.id !== id);
+    });
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+    <Container>
+      <Title text="Phonebook"></Title>
+      <Form onSubmit={addNewContact}>\</Form>
+      <Title text="Contacts"></Title>
+      <Filter text={filter} onChange={onFilterChange} />
+      <Ul>
+        <Contact
+          contacts={
+            filter === ''
+              ? contacts
+              : onActiveFilter()
+          }
+          deleteContact={deleteContact}
+        />
+      </Ul>
+    </Container>
+  ); ;
 };
+
+export default App;
